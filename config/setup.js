03fa4d2b6091859d308d01881e02a6e4695c4ec6
m-const <55905384+m-const@users.noreply.github.com>
2021-03-09 12:01:20 -0500

    Delete setup.js

--- config/setup.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    AppName: "JS Dashboard",
-    MongoURI: "mongodb+srv://app:5P^7L4h0*Q9Inh@constm4-ugzpk.mongodb.net/user?retryWrites=true&w=majority",
-    adminGroup: ["admin","This group has full access to all aspects of the application"],
-    userGroup: ["user","This group has normal user access to the application"],
-    adminUserEmail: "admin@admin"
-    
-}
